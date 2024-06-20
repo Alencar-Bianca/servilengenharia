526cbf6b0062d53baa31ff470dc29a4a1fc02fd4
Alencar-Bianca <alencar-bianca@hotmail.com>
2024-06-19 23:25:56 -0300

    banner

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './project/pages/index/index.component';
+import { ContactComponent } from './project/pages/contact/contact.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: IndexComponent
+    component: IndexComponent,
+    children: [
+      {
+        path: 'contato',
+        component: ContactComponent,
+      },
+
+    ]
   }
+      // { path: '', component: IndexComponent },
+  // { path: 'contato', component: ContactComponent },
 ];
